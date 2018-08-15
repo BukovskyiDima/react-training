847c309adfa1e8d625f05f90a8da4f4f66e082ad
dbukovskyi <dbukovskyi@exadel.com>
2018-08-15 13:13:35 +0300

    add js functions and styles

--- src/App.js
+++ src/App.js
@@ -1,42 +1,107 @@
 import React, {Component} from 'react';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
 
-  state = {
-    gifsArray: [],
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    const axios = require('axios');
+
+    axios({
+      method: 'get',
+      url: 'https://api.giphy.com/v1/gifs/trending?api_key=FipGdloXqHUVHcSHhtIBnhEmfJ89IvLv&limit=24&rating=G',
+    })
+      .then(
+        (response) => {
+          this.setState({items: response.data.data});
+        }
+      )
+  }
+
+  getTrendingGifs = () => {
+    const axios = require('axios');
+
+    axios({
+      method: 'get',
+      url: 'https://api.giphy.com/v1/gifs/trending?api_key=FipGdloXqHUVHcSHhtIBnhEmfJ89IvLv&limit=24&rating=G',
+    })
+      .then(
+        (response) => {
+          this.setState({items: response.data.data});
+        }
+      )
   };
 
-  getGifs = () => {
+  getSearchedGifs = () => {
     const axios = require('axios');
+
+    let parameter = document.querySelector('input').value;
+
     axios({
-      method:'get',
-      url:'https://api.giphy.com/v1/gifs/trending?api_key=zW3QkmvOf9ivEjRrXElFxe0C4KFd3zfO&limit=25&rating=G',
+      method: 'get',
+      url: `https://api.giphy.com/v1/gifs/search?q=` + parameter + `&api_key=FipGdloXqHUVHcSHhtIBnhEmfJ89IvLv&limit=24&rating=G`,
     })
-    .then(function(response) {
-      this.setState({gifsArray: response.data.data});
-    });
+      .then(
+        (response) => {
+          this.setState({items: response.data.data});
+        }
+      )
+  };
+
+  getRandomGifs = () => {
+    const axios = require('axios');
+    let arr = [];
+
+    axios({
+      method: 'get',
+      url: `https://api.giphy.com/v1/gifs/random?api_key=FipGdloXqHUVHcSHhtIBnhEmfJ89IvLv&limit=24&rating=G`,
+    })
+      .then(
+        (response) => {
+          arr.push(response.data.data);
+          this.setState({ items: arr });
+        }
+      )
   };
 
   render() {
+    let {items} = this.state;
     return (
       <div className="App">
         <header id="header">
           <div className="container">
             <ul className="button-holder">
               <li>
-                <a href="#" onClick={this.getGifs}>
+                <a
+                    className="btn"
+                    href="#"
+                >
                   Refresh
                 </a>
               </li>
               <li>
-                <a href="#">
+                <a
+                    className="btn"
+                    href="#"
+                    onClick={this.getRandomGifs}
+                >
                   Random
                 </a>
               </li>
               <li>
-                <a href="#">
-                  Top
+                <a
+                    className="btn"
+                    href="#"
+                    onClick={this.getTrendingGifs}
+                >
+                  Trending
                 </a>
               </li>
             </ul>
@@ -44,15 +109,28 @@ class App extends Component {
               <div className="input-holder">
                 <input type="text"/>
               </div>
-              <button>Go</button>
+              <button
+                  className="btn"
+                  onClick={this.getSearchedGifs}
+              >
+                Go
+              </button>
             </form>
           </div>
         </header>
         <main>
+          <div className="container">
+            <div className="item-holder">
+              {items.map(item => (
+                  <div className="item" key={item.id}>
+                    <img src={item.images.original.url}/>
+                    <span>{item.title}</span>
+                  </div>
+              ))}
+            </div>
+          </div>
         </main>
       </div>
     );
   }
 }
-
-export default App;
